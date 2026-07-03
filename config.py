6e20de689fc21316e9474e26d4faf07a0a568e38
hhdhfhds <mahadcodm10@gmail.com>
2026-07-03 14:02:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 
 # The email for your account (change it to your email)
-email = "example@domain.com"
+email = "mahadcodm10@gmail.com"
 # The password for your account (change it to your password) 
-password = "examplepassword"
+password = "mahadcodm10"
 # How many tokens to generate (change to specify how many tokens you want)
-amount = 1
+amount = 100
